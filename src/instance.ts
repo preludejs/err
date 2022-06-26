2a2e9703a766dc51bf4469a5eb9cfc41ec6f8f7f
Mirek Rusin <mirek@me.com>
2022-06-26 12:58:23 +0200

    Use severity if available.

--- src/instance.ts
+++ src/instance.ts
@@ -1,12 +1,24 @@
 import * as Code from './code.js'
+import * as Severity from './severity.js'
 import error from './error.js'
+import fatal from './fatal.js'
 import message from './message.js'
+import warn from './warn.js'
 
 /** @returns `Error` instance of provided `unknown` err. */
 const instance =
-  (err: unknown): Error =>
-    err instanceof Error ?
-      err :
-      error(Code.of(err), message(err))
+  (err: unknown): Error => {
+    if (err instanceof Error) {
+      return err
+    }
+    switch (Severity.of(err)) {
+      case 'warn':
+        return warn(Code.of(err), message(err))
+      case 'fatal':
+        return fatal(Code.of(err), message(err))
+      default:
+        return error(Code.of(err), message(err))
+    }
+  }
 
 export default instance
